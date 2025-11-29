1175d4102efb6662e184fcf1bcf685a7e0a0e51e
Shreyas Parab <110821677+shreyas264@users.noreply.github.com>
2025-11-29 23:35:56 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-const API_BASE = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000';
+const API_BASE =  'https://resume-analyzer-phbc.onrender.com';
 
 function App() {
   const [name, setName] = useState('');
